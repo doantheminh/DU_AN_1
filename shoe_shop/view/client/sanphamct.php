3c8afb7a56790f0d63d3b0caecb18d5bf79ff0ba
unknown <nam2003hub@gmail.com>
2022-11-28 01:07:26 +0700

    ht

--- shoe_shop/view/client/sanphamct.php
+++ shoe_shop/view/client/sanphamct.php
@@ -1,48 +1,77 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $("#binhluan").load("view/client/binhluan/binhluanform.php", {
+            idpro: <?= $id ?>
+        });
+    });
+</script>
 
-                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-                        <script>
-                        $(document).ready(function(){
-                            $("#binhluan").load("view/client/binhluan/binhluanform.php", {idpro: <?=$id?>});
-                        });
-                        </script>
-
-                <!-- single product area start -->
-                <div class="Single-product-location home2">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="location">
-                            <ul>
-                                <li><a href="index.html" title="go to homepage">Home<span>/</span></a>  </li>
-                                <li><strong>Chi tiết sản phẩm</strong></li>
-                            </ul>
-                        </div>
-                    </div>
+<!-- single product area start -->
+<div class="Single-product-location home2">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="location">
+                    <ul>
+                        <li><a href="index.html" title="go to homepage">Home<span>/</span></a> </li>
+                        <li><strong>Chi tiết sản phẩm</strong></li>
+                    </ul>
                 </div>
             </div>
         </div>
-        <!-- single product area end -->
-        <!-- single product details start -->
-        <div class="single-product-details">
-                <div class="container">
-                    <div class="row">
-                    <div class="col-lg-6">
-                        
-                        <div class="single-product-img tab-content">
+    </div>
+</div>
+<!-- single product area end -->
+<!-- single product details start -->
+<div class="single-product-details">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
 
+                <div class="single-product-img tab-content">
+
+                    <?php
+                    extract($onesp);
+                    extract($anh_mo_ta);
+                    // load ảnh mô tả 
+                
+                    ?>
+                    <div class="single-pro-main-image tab-pane active" id="pro-large-img-1">
                         <?php
-                        extract($onesp);
-                        extract($anh_mo_ta);                        
-                        ?>
-                            <div class="single-pro-main-image tab-pane active" id="pro-large-img-1">
-                            <?php
-                        $img="upload/".$img;
-                              echo  '<a href=""><img class="optima_zoom" src="'.$img.'" data-zoom-image="img/product/7.png" alt="optima"/></a>
+                        $img = "upload/" . $img;
+                        echo  '<a href=""><img class="optima_zoom" src="' . $img . '" data-zoom-image="img/product/7.png" alt="optima"/></a>
                                                               </div>
                         </div>
-                        // load ảnh mô tả 
-                        <div class="nav product-page-slider">
-                            <div class="single-product-slider">
+                                <div class="nav product-page-slider">
+                                    <div class="single-product-slider">
+                                    <div class="nav product-page-slider">
+                                    <div class="single-product-slider">
+                                        <a class="active" href="#pro-large-img-1" data-bs-toggle="tab">
+                                            <img src="view/client/img/product/28.png" alt="" >
+                                        </a>
+                                    </div>
+                                    <div class="single-product-slider">
+                                        <a href="#pro-large-img-2" data-bs-toggle="tab">
+                                            <img src="view/client/img/product/28.png" alt="" >
+                                        </a>
+                                    </div>
+                                    <div class="single-product-slider">
+                                        <a href="#pro-large-img-3" data-bs-toggle="tab">
+                                            <img src="view/client/img/product/28.png" alt="" >
+                                        </a>
+                                    </div>
+                                    <div class="single-product-slider">
+                                        <a href="#pro-large-img-4" data-bs-toggle="tab">
+                                            <img src="view/client/img/product/28.png" alt="" >
+                                        </a>
+                                    </div>
+                                    <div class="single-product-slider">
+                                        <a href="#pro-large-img-5" data-bs-toggle="tab">
+                                            <img src="view/client/img/product/28.png" alt="" >
+                                        </a>
+                                    </div>
+                                </div>
                                 <a class="active" href="#pro-large-img-1" data-bs-toggle="tab">
                                     <img src="view/client/img/product/28.png" alt="">
                                 </a>
@@ -53,18 +82,18 @@
                     </div>
                     <div class="col-lg-6">
                         <div class="single-product-details">
-                            <a href="#" class="product-name">'.$name.'</a>
+                            <a href="#" class="product-name">' . $name . '</a>
                            
 
                             <div class="item-price">
                            
-                                    <p><span>$'.$price.'</span></p>
+                                    <p><span>$' . $price . '</span></p>
                                
                                 
                             </div>
                             <div class="single-product-info">
                                
-                                     <p>'.$mota.'</p>
+                                     <p>' . $mota . '</p>
                              
                                 
                                 <div class="share">
@@ -125,10 +154,10 @@
 
                                     <div class="actions">
                                                     <form action="index.php?act=addtocart" method="post">
-                                                      <input type="hidden" name="id" value="'.$id.'">
-                                                      <input type="hidden" name="name" value="'.$name.'">
-                                                      <input type="hidden" name="img" value="'.$img.'">
-                                                      <input type="hidden" name="price" value="'.$price.'">
+                                                      <input type="hidden" name="id" value="' . $id . '">
+                                                      <input type="hidden" name="name" value="' . $name . '">
+                                                      <input type="hidden" name="img" value="' . $img . '">
+                                                      <input type="hidden" name="price" value="' . $price . '">
                                                       <input type="submit" value="Mua" name="addtocart" class="btn btn-dark">
                                                         </form>
                                                    </div>
@@ -143,85 +172,85 @@
                               ';
                         ?>
 
-        <!-- single product details end -->
-        <!-- single product tab start -->
-        <div class="single-product-tab-area">
+                        <!-- single product details end -->
+                        <!-- single product tab start -->
+                        <div class="single-product-tab-area">
 
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="single-product-tab">
-                            <ul class="nav single-product-tab-navigation" role="tablist">
-                                <li role="presentation">
-                                    <a class="active" href="#tab1" aria-controls="tab1" role="tab" data-bs-toggle="tab">Product Description</a>
-                                </li>
-                                <li role="presentation">
-                                    <a href="#tab2" aria-controls="tab2" role="tab" data-bs-toggle="tab">reviews</a>
-                                </li>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="single-product-tab">
+                                            <ul class="nav single-product-tab-navigation" role="tablist">
+                                                <li role="presentation">
+                                                    <a class="active" href="#tab1" aria-controls="tab1" role="tab" data-bs-toggle="tab">Product Description</a>
+                                                </li>
+                                                <li role="presentation">
+                                                    <a href="#tab2" aria-controls="tab2" role="tab" data-bs-toggle="tab">reviews</a>
+                                                </li>
 
-                            </ul>
+                                            </ul>
 
-                            <!-- Tab panes -->
-                            <div class="tab-content single-product-page">
-                                <div role="tabpanel" class="tab-pane fade show active" id="tab1">
-                                    <div class="single-p-tab-content">
-                                        <p><?=$mota?></p>
-                                    </div>
-                                </div>
-                                <div role="tabpanel" class="tab-pane fade" id="tab2">
-                                    <div class="single-p-tab-content">
-                                        <div class="row" id="binhluan">
-                                            <div class="col-md-5">
-                    
+                                            <!-- Tab panes -->
+                                            <div class="tab-content single-product-page">
+                                                <div role="tabpanel" class="tab-pane fade show active" id="tab1">
+                                                    <div class="single-p-tab-content">
+                                                        <p><?= $mota ?></p>
+                                                    </div>
+                                                </div>
+                                                <div role="tabpanel" class="tab-pane fade" id="tab2">
+                                                    <div class="single-p-tab-content">
+                                                        <div class="row" id="binhluan">
+                                                            <div class="col-md-5">
+
+                                                            </div>
+
+                                                        </div>
+                                                    </div>
+                                                </div>
+
                                             </div>
-
                                         </div>
                                     </div>
                                 </div>
-
                             </div>
                         </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- single product tab end -->
-        <!-- upsell product area start-->
+                        <!-- single product tab end -->
+                        <!-- upsell product area start-->
 
-        <!-- upsell product area end-->
-        <!-- related product area start-->
-        <div class="related-product home2">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="product-title">
-                            <h2>Sản phẩm cùng loại</h2>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="related-slider">
-                       
-                        <?php
-                            foreach ($sp_cung_loai as $sp_cung_loai) {
-                                extract($sp_cung_loai);
-                                $linksp="index.php?act=sanphamct&idsp=".$id;
-                                $img="upload/".$img;
+                        <!-- upsell product area end-->
+                        <!-- related product area start-->
+                        <div class="related-product home2">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="product-title">
+                                            <h2>Sản phẩm cùng loại</h2>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="related-slider">
 
-                                echo '
+                                        <?php
+                                        foreach ($sp_cung_loai as $sp_cung_loai) {
+                                            extract($sp_cung_loai);
+                                            $linksp = "index.php?act=sanphamct&idsp=" . $id;
+                                            $img = "upload/" . $img;
+
+                                            echo '
                                 <div class="col-md-12">
                                 <div class="single-product">
                                 <div class="product-img"> 
-                                <a href="'.$linksp.'">
-                                            <img src="'.$img.'" alt="" class="primary-img">
-                                            <img src="'.$img.'" alt="" class="secondary-img"></a> 
+                                <a href="' . $linksp . '">
+                                            <img src="' . $img . '" alt="" class="primary-img">
+                                            <img src="' . $img . '" alt="" class="secondary-img"></a> 
                                 </div>
                                 <div class="product-price">
                                     <div class="product-name">
-                                        <a href="single-product.html" title="Fusce aliquam">'.$name.'</a>
+                                        <a href="single-product.html" title="Fusce aliquam">' . $name . '</a>
                                     </div>
                                     <div class="price-rating">
-                                        <span>'.$price.'</span>
+                                        <span>' . $price . '</span>
                                     </div>
                                     
                                 </div>
@@ -229,11 +258,11 @@
                             </div> 
                             </div>
                             ';
-                            }
-                       ?>
-                        
-                        
-                    </div>
-                </div>
-            </div>
-        </div>
+                                        }
+                                        ?>
+
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
